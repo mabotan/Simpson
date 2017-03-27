f50e89fa9638309fd7895cea2fd37c8619d5efcf
mabotan <mmymoddum@gmail.com>
2017-03-27 21:04:38 +0700

    Greenfoot 10
    create monter folder for random motion

---
+++ MonHead.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MonHead here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MonHead extends Monster
+{
+    /**
+     * Act - do whatever the MonHead wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
